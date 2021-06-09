db376cae1a842be4c34ad9653da783fad179f135
Cloud User <centos@ip-172-31-7-126.eu-west-2.compute.internal>
2021-06-09 11:11:02 +0000

    A complete Ex6 solution

--- Ex6/top.v
+++ Ex6/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #6 - RGB Colour Converter
-// Student Name:
-// Date: 
+// Student Name: Kian Hocking
+// Date: 09/06/2021
 //
 //
 //  Description: In this exercise, you need to design a memory with 8 entries, 
@@ -15,3 +15,24 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module colour_converter(clk, colour, enable, rgb);
+
+input clk, enable;
+input[2:0] colour;
+output[23:0] rgb;
+
+
+//----------- Begin Cut here for INSTANTIATION Template ---// INST_TAG
+blk_mem_gen_0 your_instance_name (
+  .clka(clk),    // input wire clka
+  .ena(enable),      // input wire enable
+  .wea(wea),      
+  .addra(colour),      // input wire [2:0] colour
+  .dina(0),    // input wire [23 : 0] dina
+  .douta(rgb)  // output wire [23 : 0] douta
+);
+// INST_TAG_END ------ End INSTANTIATION Template ---------
+
+endmodule
--- Ex6/top_tb.v
+++ Ex6/top_tb.v
@@ -1,9 +1,96 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #6 - RGB Colour Converter
-// Student Name:
-// Date: 
+// Student Name: Kian Hocking
+// Date: 09/06/2021
 //
 // Description: A testbench module to test Ex6 - RGB Colour Converter
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+module colour_converter_tb();
+
+parameter CLK_PERIOD = 10;
+
+reg clk, err, enable;
+reg[2:0] colour;
+wire[23:0] rgb; 
+reg[23:0] prev_rgb;
+
+//clock generation
+initial
+    begin
+       clk = 1'b1;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+     end
+
+//section to initialise variables and turn on enable after a little while
+initial begin
+
+	err = 0;
+	enable = 0;
+	colour = 0;	
+    prev_rgb = 0;
+    
+    // start with enable off for a bit to allow testing
+    #(CLK_PERIOD*24)
+    enable = 1;
+	
+end
+
+//section to increment the colour value 
+initial begin
+    forever begin
+        #(CLK_PERIOD*8)
+		colour <= colour + 1;
+    end
+end
+
+//section to check errors
+initial begin
+
+    #(CLK_PERIOD*4) // delays error checking to account for delay retrieving the rgb value from the block memory
+
+    forever begin
+    
+        #(CLK_PERIOD*8)
+	    
+	    //checking if the rgb value changes when the colour value changes
+		if((enable)&&(prev_rgb  == rgb)) begin
+		          $display("Test Failed, rgb not changing when enable is active");
+		          err = 1;
+		end
+		
+		//checking if the rgb value remains fixed when enable is not active
+		else if ((!enable)&&(prev_rgb != rgb)) begin
+                $display("Test Failed, rgb changed when enable is not active");
+                err = 1;
+        end
+    
+        //sets the previous rgb value to the rgb value for error checking at the next rgb value.
+		prev_rgb = rgb;
+		
+	end
+    
+end
+
+//Finish test, check for success
+initial begin
+        #(80*CLK_PERIOD)
+        
+        if (err==0) begin
+          $display("***TEST PASSED! :) ***");
+        end
+        
+        $finish;
+end
+
+
+colour_converter top(
+    .clk (clk), 
+    .enable (enable), 
+    .colour (colour), 
+    .rgb (rgb)
+    );
+
+endmodule
